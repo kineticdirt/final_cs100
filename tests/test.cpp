5aba8461ab19a18eba73b35bce6421be02e6b228
DanielKoKo <danielko1202@gmail.com>
2021-05-17 20:08:22 -0700

    Deleted unused test.cpp

--- tests/test.cpp
+++
@@ -1,9 +0,0 @@
-#include "../tests/Book_test.hpp"
-#include "gtest/gtest.h"
-#include "../header/library.hpp"
-#include "../header/user.hpp"
-
-int main(int argc, char **argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
-}
