b76d98b2e04d8fa80135b137373022cf9d5d4b40
aungthuhein13 <henryaungthuhein@gmail.com>
2021-05-28 20:04:22 -0700

    Modified Book Class

--- header/Book.hpp
+++ header/Book.hpp
@@ -2,29 +2,32 @@
 #define __BOOK__HPP___
 
 #include <string>
-#include "BookComponent.hpp"
-#include <iostream>
+
 using namespace std;
 
-class Book : public BookComponent{
-private:
+class Book {
+private:  
 	string name;
 	string author;
+	string isbn;
+	string subgenre;
 
 public:
-	Book(string name, string author) : BookComponent() {
+	Book(string name, string author,string isbn,string subgenre){
 		this->name = name;
 		this->author = author;
+		this->isbn = isbn;		
+		this->subgenre = subgenre;
 	}
 
 	~Book() {};
 
 	string getname() const { return name; }
-
+	string getIsbn() const { return isbn; }	
 	string getauthor() const { return author; }
-
-	void display(ostream& out) const {
-		out << "\t" << "Book: " << name << " by " << author << endl;
+	string getsubgenre() const { return subgenre; }
+	void display() const {
+		cout << "Book: " << name << " by " << author << endl;
 	}
 };
 
