7a6b33394219b9fb46ce93f7e76642121c769903
DanielKoKo <danielko1202@gmail.com>
2021-05-30 12:25:06 -0700

    User class can now print user debt

--- header/user.hpp
+++ header/user.hpp
@@ -12,13 +12,15 @@ class User
     private:
 	string username;
 	string password;
+	int debt;
 	
     public:
-	User(string, string);
+	User(string, string, int);
 	bool initialize();              //handles running user class until user logs out
 	char user_menu();               //prints available user menus	
-        void view_my_info();            //view current user information
+        void view_debt();               //view current user debt
 	void view_books();              //view a list of available books
+	int get_debt() const;
 	string get_username() const; 
 	string get_password() const;
 };
