ba7c53743cd6416e6342a6a904b6b3bbe070e580
aalla009 <31803912+kineticdirt@users.noreply.github.com>
2021-05-31 13:24:02 -0700

    Updated Admin.cpp

--- src/Admin.cpp
+++ src/Admin.cpp
@@ -1,24 +1,53 @@
-#include "../header/admin.hpp"
 
-Admin::Admin(string username, string password) : User(username, password){}
-/*
+#include "../header/admin.hpp"
+#include <fstream>
+#include <iostream>
+#include <string>
+
+Admin::Admin(string user, string pass) : username{user}, password{pass}
+{
+	ofstream outFile;
+	outFile.open("admin_account.txt", std::ios::app);
+    if (!inFile.is_open())
+	{
+		cout << "admin_accounts.txt" << " does not exist!" << endl;
+    } 	else {
+		cout << get_username() << " : " << get_password() << endl;
+	}
+}
+
 bool User::initialize()
 {
     char choice = ' ';
     choice = user_menu();
     if (choice == '1')
     {
-	    view_books();
+	view_books();
     }
-    else  //if user enters '0'
+    else if (choice == '3')
     {
-	    return false;
+	borrow_a_book();
+    }
+    else if (choice == '4')
+    {
+		view_debt();
+    }
+    else if (choice == '5')
+    {
+		view_borrowed();
+    }
+    else if (choice == '0')
+    {
+	return false;
+    }
+    else if (choice == ' ') 
+    {
+		admin_menu();
     }
     
     return true;
 }
-*/
-/*char Admin::admin_menu()
+char Admin::admin_menu()
 {
     char choice = ' ';
    
@@ -30,30 +59,26 @@ bool User::initialize()
     cout << "2. Search Books" << endl;
     cout << "3. Assign book to User" << endl;
     cout << "4. View Users" << endl;
+	  cout << "5. View Users" << endl;
     cout << "0. Main Menu" << endl;
 
    
-    while (choice != '1' && choice != '0')
+   while (choice != '1' && choice != '0')
     {
  	cin >> choice;
 
-	if (choice == '1' || choice == '0')
+	if (choice == '1' || choice == '0' || choice == '2' || choice == '3' || choice == '4' || choice == '5')
 	{
 	    return choice;
 	}
 	else
 	{
 	    cout << "Enter a valid menu option!" << endl;
-	    cin.clear();
+	    return ' ';
 	}
     }
 }
-*/
-void Admin::view_info()
-{
-    cout << "\nUsername: " << get_username() << endl;
-    cout << "Password: " << get_password() << endl;
-}
+
 
 string Admin::get_username() const 
 {
@@ -64,16 +89,29 @@ string Admin::get_password() const
 {
     return password;
 }
-
-/*void Admin::view_books()
-/*
-void Admin::view_books()
-
-{
-    stringstream out;
-    cout << "\nAvailable Books: " << endl;
-    avail_books->display(out);    
-
-    cout << out.str() << endl;
+void Admin::borrow_user_book(string passwrd, string user_username, string title) {
+	if(passwrd == getPassword()) {
+		fstream file;
+		file.open("borrowed_books.txt",std::fstream::in | std::fstream::out | std::fstream::app);
+		if(!file.is_open()) {
+			cout << "Failed to open borrowed_books" << endl;
+			return;
+		}
+		string* checker;
+		while(getline(file, checker, ' '){
+			string title1;
+			if(user_username.compare(*checker)) {
+				file >> title1;
+				if(title1.compare(title)) {
+					return;
+			}
+		}
+		file << user_username<<" " << title << endl;
+        file.close(); 
+	} else { 
+		cout << "incorrect password, reenter correct password"
+		string passwrd1;
+		cin >> passwrd1; 
+		borrow_user_book(passwrd1, user_username, title);
+	}
 }
-*/
