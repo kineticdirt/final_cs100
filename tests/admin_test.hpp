e73abfd3fe1a09bb5fab8f4043e01e688e2c6987
aalla009 <31803912+kineticdirt@users.noreply.github.com>
2021-05-31 13:24:49 -0700

    update admin_test.hpp

--- tests/admin_test.hpp
+++ tests/admin_test.hpp
@@ -1,20 +1,27 @@
-
 #ifndef __ADMIN__TEST_HPP__
 #define __ADMIN__TEST_HPP__
 
 #include "gtest/gtest.h"
 #include "../header/admin.hpp"
 #include "../header/library.hpp"
+#include "../header/admin.hpp"
+#include "../src/Admin.cpp
 #include "../header/Book.hpp"
-
+#include <ifstream>
 TEST(AdminTest, UsernameTest) {
     Admin test = Admin("username", "password");
     EXPECT_EQ(test.get_username(), "username"); 
 }
-
-TEST(UserTest, PasswordTest) {
+TEST(AdminTest, PasswordTest) {
     Admin test = Admin("username", "password");
     EXPECT_EQ(test.get_password(), "password");
 }
-
+TEST(AdminTest, BookTest) {
+    Admin test = Admin("username", "password");
+    test.borrow_user_book("password", "wholesome" , "delete_this");
+    ifstream inputFile("borrowed_books.txt")
+    string s1;
+    while(inputFile >> s1);
+    EXPECT_EQ(s1, "delete_this");
+}
 #endif //__ADMIN__TEST_HPP__
